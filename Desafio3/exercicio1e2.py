1046b5234a2a24a1c044b53b42a90d58cbf02f07
Regiane Melo <regianemelo1995@gmail.com>
2023-12-20 16:50:27 -0300

    Resolução desafio 3

--- Desafio3/exercicio1e2.py
+++ Desafio3/exercicio1e2.py
@@ -1,11 +1,66 @@
-meu_numero = 18
-nome_comediante = "Afonso Padilha"
-
-if meu_numero % 2 == 0:
-    print(f'O numero {meu_numero} é par.')
-else:
-    print('O número é impar.')
-
-numero_letras = len(nome_comediante)
-print(f'O nome {nome_comediante} tem {numero_letras} letras.')
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "18\n"
+     ]
+    }
+   ],
+   "source": [
+    "meu_numero = 18\n",
+    "nome_comediante = \"Afonso Padilha\"\n",
+    "print(meu_numero)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "Afonso Padilha\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(nome_comediante)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
